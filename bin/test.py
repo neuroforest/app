f483dc5548b63c411043c65aadde0ba0bb3e8339
Urban Alič <urbanalich@gmail.com>
2026-02-13 09:06:18 +0100

    UPDATE: script bin/test.py

--- bin/test.py
+++ bin/test.py
@@ -2,7 +2,6 @@
 Test NeuroForest app
 """
 
-import contextlib
 import logging
 import os
 import subprocess
@@ -14,19 +13,14 @@ from rich.console import Console
 from neuro.utils import terminal_style, internal_utils
 
 
-@contextlib.contextmanager
-def chdir(path):
-    old_dir = os.getcwd()
-    os.chdir(path)
-    try:
-        yield
-    finally:
-        os.chdir(old_dir)
+def reset_submodule(path, branch):
+    with internal_utils.chdir(path):
+        subprocess.run(["git", "fetch", "origin"], check=True)
+        subprocess.run(["git", "reset", "--hard", f"origin/{branch}"], check=True)
+        subprocess.run(["git", "clean", "-fdx"], check=True)
 
 
-def prepare_neuro():
-    from neuro.utils import config  # noqa: F401
-
+def prepare_neuro_local():
     rsync_command = [
         "rsync",
         "-va",
@@ -53,25 +47,51 @@ def prepare_neuro():
         "--hard",
         "9e6a53755"
     ]
-    with chdir(internal_utils.get_path("tw5")):
+    with internal_utils.chdir(internal_utils.get_path("tw5")):
         with Console().status("[bold] reset tw5...", spinner="dots"):
             subprocess.run(checkout_tw5_command, check=True, capture_output=True)
     print(f"{terminal_style.SUCCESS} Reset tw5")
 
 
-def test_neuro():
-    if len(sys.argv) > 1:
-        pytest_args = sys.argv[1:]
+def test_neuro(mode, pytest_args):
+    if mode == "local":
+        prepare_neuro_local()
+    elif mode == "develop":
+        reset_submodule(internal_utils.get_path("app") + "/neuro", "develop")
+    elif mode == "master":
+        reset_submodule(internal_utils.get_path("app") + "/neuro", "master")
     else:
+        print("Mode not supported:", mode)
+
+    if len(pytest_args) == 0:
         pytest_args = ["neuro/tests"]
     pytest.main(pytest_args)
 
 
-if __name__ == "__main__":
-    os.environ["ENVIRONMENT"] = "TESTING"
-    logging.basicConfig(level=logging.INFO)
-    app_path = os.path.dirname(os.path.dirname(__file__))
-    with chdir(app_path):
-        prepare_neuro()
-        test_neuro()
+def parse_arguments():
+    args = sys.argv[1:]
 
+    if len(args) == 0:
+        return "local", []
+    elif args[0] in ["-l", "--local"]:
+        return "local", args[1:]
+    elif args[0] in ["-d", "--develop"]:
+        return "develop", args[1:]
+    elif args[0] in ["-m", "--master"]:
+        return "master", args[1:]
+    else:
+        return "local", args
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    mode, pytest_args = parse_arguments()
+    os.environ["ENVIRONMENT"] = "TESTING"
+    app_path = os.getenv("NF_DIR")
+    with internal_utils.chdir(app_path):
+        from neuro.utils import config  # noqa: F401
+        test_neuro(mode, pytest_args)
+
+
+if __name__ == "__main__":
+    main()
