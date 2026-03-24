b5e141796037767ab3bae0a317ce4a591aa276cc
Urban Alič <urbanalich@gmail.com>
2026-03-24 13:53:59 +0100

    MIGRATE: third-party plugin configuration

--- tw5-plugins/kookma/build.sh
+++ tw5-plugins/kookma/build.sh
@@ -6,3 +6,5 @@ cp -r TW-Shiraz/source/shiraz compiled/
 cp -r TW-Shiraz/source/shiraz-callout compiled/
 cp -r TW-Shiraz/source/shiraz-formatter compiled/
 cp -r TW-Todolist/source/todolist compiled/
+cp -r overrides/commander/* compiled/commander/
+cp -r overrides/shiraz/* compiled/shiraz/
