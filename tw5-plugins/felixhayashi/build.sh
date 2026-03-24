13ead235628ad2e1dde92980bcdf17ad23fc737e
Urban Alič <urbanalich@gmail.com>
2026-03-24 12:30:00 +0100

    UPDATE: tw5-plugins

---
+++ tw5-plugins/felixhayashi/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+mkdir -p compiled
+cp -r TW5-HotZone/dist/felixhayashi/hotzone compiled/
---
+++ tw5-plugins/tobibeer/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+mkdir -p compiled
+cp -r tw5-appear/plugins/tobibeer/appear compiled/
+cp -r tw5-preview/plugins/tobibeer/preview compiled/
