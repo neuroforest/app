2cf4b09a0bf4d27fc404b959fd89c559dc808e80
Urban Alič <urbanalich@gmail.com>
2026-03-24 08:22:27 +0100

    UPDATE: tw5-plugins

---
+++ tw5-plugins/kookma/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+mkdir -p compiled
+cp -r TW-Commander/source/commander compiled/
+cp -r TW-Shiraz/source/shiraz compiled/
+cp -r TW-Shiraz/source/shiraz-callout compiled/
+cp -r TW-Shiraz/source/shiraz-formatter compiled/
+cp -r TW-Todolist/source/todolist compiled/
--- tw5-plugins/neuroforest/build.sh
+++ tw5-plugins/neuroforest/build.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 set -e
-python3 -c "
-import json, os, shutil, sys
+mkdir -p compiled
 
-wrapper = sys.argv[1]
-with open(os.path.join(wrapper, 'build.json')) as f:
-    submodules = json.load(f)['submodules']
-
-compiled = os.path.join(wrapper, 'compiled')
-for name in submodules:
-    src = os.path.join(wrapper, name, 'source')
-    dest = os.path.join(compiled, name)
-    shutil.copytree(src, dest)
-
-    info_path = os.path.join(dest, 'plugin.info')
-    with open(info_path) as f:
-        info = json.load(f)
-    info['plugin-priority'] = 100
-    with open(info_path, 'w') as f:
-        json.dump(info, f, indent=4)
-        f.write('\n')
-" "$(pwd)"
+for dir in basic core front mobile neo4j-syncadaptor; do
+    cp -r "$dir/source" "compiled/$dir"
+    sed -i '1a\\t"plugin-priority": 100,' "compiled/$dir/plugin.info"
+done
